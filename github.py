710a40537caaf21e4ee703044314cb67a4bbfd1d
seem <mwlorgat@gmail.com>
2020-06-03 11:50:02 +0200

    Clean-up

--- github.py
+++ github.py
@@ -1,4 +1,5 @@
 import os
+import re
 import subprocess
 import sys
 
@@ -6,106 +7,98 @@ from workflow import Workflow3
 
 
 WORKSPACE_DIR = os.path.expanduser(os.environ.get("ALFRED_WORKSPACE_DIR", "~/code"))
-GITHUB_URL = "https://github.com/{}"
 
 
-def main(wf):
-    query = wf.args[0] if wf.args else None
-    # TODO: Smarter caching? Only on file changes...?
-    # TODO: Can Alfred itself handle caching and/or fitlering for us?
-    #       What do we gain by handling it ourselves?
-    repos = wf.cached_data("repos", get_repos, max_age=600)
-    if query:
-        repos = wf.filter(query, repos, key=get_repo_search_key)
-    for repo in repos:
-        title = "/".join([repo["author"], repo["name"]])
-        wf.add_item(
+class Repository:
+    def __init__(self, author, name):
+        self.author = author
+        self.name = name
+
+    GITHUB_URL = "https://github.com/{}/{}"
+
+    @property
+    def url(self):
+        return self.GITHUB_URL.format(self.author, self.name)
+
+    @property
+    def search_key(self):
+        return u" ".join(
+            [
+                self.author,
+                self.name,
+            ]
+        )
+
+    @classmethod
+    def from_dir(cls, path):
+        if not os.path.isdir(path):
+            raise ValueError("{} is not a directory".format(path))
+        try:
+            with open(os.devnull, "wb") as devnull:
+                output = subprocess.check_output(
+                    ["git", "remote", "-v"],
+                    cwd=path,
+                    stderr=devnull,
+                )
+        except subprocess.CalledProcessError:
+            raise ValueError("{} is not a git repository".format(path))
+        author, name = Repository._parse_git_remote(output.decode())
+        if name.endswith(".git"):
+            name = name[:-4]
+        return cls(author, name)
+
+    GIT_REMOTE_PATTERN = re.compile(
+        r"^origin\sgit@github\.com:(.*)\/(.*)\s\((?:fetch|push)\).*$"
+    )
+
+    @classmethod
+    def _parse_git_remote(cls, string):
+        lines = string.split("\n")
+        for line in lines:
+            line = line.lower().strip()
+            match = cls.GIT_REMOTE_PATTERN.match(line)
+            if match:
+                return match.groups()
+        raise ValueError("string has no 'origin' remote")
+
+    def as_alfred_item(self):
+        title = "{}/{}".format(self.author, self.name)
+        return dict(
             title=title,
-            subtitle=repo["url"],
-            arg=repo["url"],
+            subtitle=self.url,
+            arg=self.url,
             valid=True,
         )
-    wf.send_feedback()
-
-
-def get_repo_search_key(repo):
-    return u" ".join(
-        [
-            repo["author"],
-            repo["name"],
-        ]
-    )
 
 
 def get_repos(workspace_dir=WORKSPACE_DIR):
     repos = []
-    for file_name in os.listdir(workspace_dir):
-        path = os.path.join(workspace_dir, file_name)
-
-        if not os.path.isdir(path):
-            continue
-
-        # Get subtitle from readme
-        # readme_path = os.path.join(path, "README.md")
-        # if os.path.isfile(readme_path):
-        #     with open(readme_path) as f:
-        #         line = ""
-        #         while True:
-        #             try:
-        #                 line = next(f).strip()
-        #             except StopIteration:
-        #                 break
-        #             if line and not (
-        #                 line.startswith("#")
-        #                 or line.startswith("![")
-        #                 or line.startswith("[![")
-        #             ):
-        #                 break
-        #         subtitle = line
-        # else:
-        #     subtitle = ""
-
-        # Get the github url
-        # git_folder = os.path.join(path, ".git")
-        # if not os.path.isdir(git_folder):
-        #     continue
-
+    for d in os.listdir(workspace_dir):
+        d = os.path.join(workspace_dir, d)
         try:
-            output = subprocess.check_output(["git", "remote", "-v"], cwd=path)
-        except subprocess.CalledProcessError:
-            continue
-
-        lines = output.split("\n")
-        for line in lines:
-            if line.startswith("origin"):
-                github_suffix = line.split()[1].split(":")[1]
-                break
+            repo = Repository.from_dir(d)
+        except ValueError:
+            pass
         else:
-            # no origin remote
-            continue
-
-        url = GITHUB_URL.format(github_suffix)
-        if url.endswith(".git"):
-            url = url[:-4]
-        # git_config_path = os.path.join(git_folder, "config")
-        # config = ConfigParser.ConfigParser()
-        # print(git_config_path)
-        # with open(git_config_path) as f:
-        #     text = "".join(l.lstrip() for l in f.read())
-        #     config = config.read(text)
-
-        # item = {
-        #     "title": file_name,
-        #     "subtitle": subtitle,
-        # }
-
-        author = github_suffix.split("/")[0].lower()
-
-        # title = "/".join([author, file_name])
-
-        repos.append({"author": author, "name": file_name, "url": url})
+            repos.append(repo)
     return repos
 
 
-wf = Workflow3()
-sys.exit(wf.run(main))
+def main(wf):
+    query = wf.args[0] if wf.args else None
+    repos = []
+    for d in wf.cached_data("repos", get_repos, max_age=60*60*24):
+        # TODO: Why is url even there, but search_key isn't?
+        d.pop("url")
+        repo = Repository(**d)
+        repos.append(repo)
+    if query:
+        repos = wf.filter(query, repos, lambda x: x.search_key)
+    for repo in repos:
+        wf.add_item(**repo.as_alfred_item())
+    wf.send_feedback()
+
+
+if __name__ == "__main__":
+    wf = Workflow3()
+    sys.exit(wf.run(main))
